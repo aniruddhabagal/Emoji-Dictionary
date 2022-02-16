5d4596b4f67e96148c14f0c52cb1b4977e9e3139
Aniruddha Bagal <bagalaniruddha@gmail.com>
2022-02-16 23:49:18 +0530

    Adding Emoji-Dictionary

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import React, { useState } from "react";
 
-function App() {
+const emojiDictionary = {
+  "😀": "Happy",
+  "😑": "Frustrated",
+  "🥰": "Loved",
+  "😒": "Annoyed",
+  "🙂": "Smiling",
+  "🤦🏻": "Disbelief",
+  "❤️": "Love"
+};
+
+let emojis = Object.keys(emojiDictionary);
+
+export default function App() {
+  const [meaning, setMeaning] = useState("Enter the Emoji");
+
+  function inputHandler(event) {
+    let userInput = event.target.value;
+
+    let meaning = emojiDictionary[userInput];
+
+    // if (meaning === undefined) {
+    //   meaning = "Not in Database";
+    // }
+    // else if (meaning === undefined) {
+    //   meaning = "Currently not in the Database";
+    // }
+
+    // console.log(meaning);
+    setMeaning(meaning);
+  }
+
+  function clickHandler(emoji) {
+    // console.log(emoji);
+    let meaning = emojiDictionary[emoji];
+    setMeaning(meaning);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Emoji Dictionary!</h1>
+
+      <input onChange={inputHandler} />
+
+      <h2>{meaning}</h2>
+      <div>
+        <h3>Emojis We Know: </h3>
+        {emojis.map(function (emoji) {
+          return (
+            <span
+              onClick={() => clickHandler(emoji)}
+              style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+              key={emoji}
+            >
+              {emoji}
+            </span>
+          );
+        })}
+      </div>
     </div>
   );
 }
-
-export default App;
